972d1d4bc32c88cb930810739565bce6da072fad
Vanderlei Neto <jv.neto@outlook.com.br>
2024-11-26 13:52:10 -0300

    Ajustando

--- server.js
+++ server.js
@@ -9,6 +9,49 @@ const PORT = process.env.PORT || 3000; // Corrigido aqui
 // Habilitar CORS
 app.use(cors());
 
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// })
+app.get('/', (req, res) => {
+  const htmlContent = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>How to Use the API</title>
+      <style>
+        body {
+          font-family: Arial, sans-serif;
+          margin: 0;
+          padding: 20px;
+          background-color: #f4f4f4;
+        }
+        h1 {
+          color: #333;
+        }
+        p {
+          color: #666;
+        }
+        code {
+          background-color: #eee;
+          padding: 2px 4px;
+          border-radius: 4px;
+        }
+      </style>
+    </head>
+    <body>
+      <h1>How to Use the API</h1>
+      <p>To use this API, you can make a GET request to the following endpoint:</p>
+      <ul>
+          <li><a href="https://cutterapi.vercel.app/api/data">/api/data</a></li>
+      </ul>
+    </body>
+    </html>
+  `;
+  res.send(htmlContent);
+});
+
 app.get('/api/data', (req, res) => {
   const filePath = path.join(__dirname, 'cutter.csv');
   const csvData = fs.readFileSync(filePath, 'utf8');
